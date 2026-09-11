2cebc52edd89de0aadd7884b4424c1f3f73920ec
padatta-bmc <pamela.bmc2026@gmail.com>
2026-09-11 14:35:36 +0530

    Updated docusaurus config

--- company-docs/docusaurus.config.js
+++ company-docs/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Training Documentation',
-  tagline: 'Guides, References, and Resources',
+  title: 'Pamela DocHub',
+  tagline: 'Learning, building, documenting',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://padatta-bmc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/newrepo/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'padatta-bmc', // Usually your GitHub org/user name.
+  projectName: 'doc-as-code-training', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -91,9 +91,9 @@ const config = {
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'productASidebar',
+            sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Documentation',
+            label: 'Tutorial',
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
