df864d63f70509d346f3e27359596c8c8de90fc2
padatta-bmc <pamela.bmc2026@gmail.com>
2026-09-11 17:03:26 +0530

    Updated Index

--- company-docs/src/components/HomepageFeatures/index.js
+++ company-docs/src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Collaboration First',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Bringing together SMEs, Support, 
+        Engineering, and Documentation to create better user experiences
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Insights Into Action',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Using analytics, feedback, and research
+        to make every page more valuable.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Designing Better Experiences',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Creating content that helps users succeed faster,
+        with less friction and greater confidence.
       </>
     ),
   },
