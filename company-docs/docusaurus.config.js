860372cef57dfa6150ac66e025074935b1a8f391
padatta-bmc <pamela.bmc2026@gmail.com>
2026-09-11 15:28:12 +0530

    Updated url name

--- company-docs/docusaurus.config.js
+++ company-docs/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://padatta-bmc.github.io/Doc-as-code-training/',
+  url: 'https://padatta-bmc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Doc-as-code-training/',
